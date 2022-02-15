89c75f1422845e0b7a95528003645e36f083a14f
Varun Chari <charivarun@gmail.com>
2022-02-14 19:41:08 -0800

    added support for Kasa smartplug

--- pi/home_auto_client.py
+++ pi/home_auto_client.py
@@ -4,14 +4,28 @@ import time
 import os
 import globals
 import datetime
+import asyncio
+from kasa import SmartPlug
 
 def start_sub(client,topic):
     mqttClient.subscribe(client,topic)
     client.loop_forever()
 
+async def plug_ops():
+    if dev.is_on:
+        if (("mask_was_worn_incorrectly" in globals.recieved_message) or ("without_mask" in globals.recieved_message)):
+            await dev.turn_off()
+            await dev.update()
+    if dev.is_off:
+        if ("with_mask" in globals.recieved_message):
+            await dev.turn_on()
+            await dev.update()
+
 globals.init()
 client = mqttClient.connect_mqtt()
 topic = "darknet/predictions"
+dev = SmartPlug("192.168.1.68")
+asyncio.run(dev.update())
 Thread(target=start_sub, args=(client,topic)).start()
 
 try:
@@ -26,6 +40,7 @@ while True:
         try:
             file_output.write(str(cur_time) + ":" + globals.recieved_message + "\n")
             file_output.flush()
+            asyncio.run(plug_ops())
         except:
             print("could not write to file")
         globals.recieved_message = ""
